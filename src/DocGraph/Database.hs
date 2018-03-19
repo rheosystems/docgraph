e97b8a8faed539e054e010ecbf0724955fb57e32
Mikkel Christiansen <mikkel@rheosystems.com>
2018-03-19 13:39:39 +0200

    Added query for selecting documents

--- src/DocGraph/Database.hs
+++ src/DocGraph/Database.hs
@@ -34,15 +34,36 @@ insertDocument doc = runDB $ query doc q
   where
     q :: Query Document Int64
     q = statement sql encoder decoder True
+
     sql = "insert into documents(title, author) values ($1, $2) returning document_id"
+
+    encoder :: E.Params Document
     encoder = contramap documentTitle (E.value E.text) <>
               contramap documentAuthor (E.value E.text)
+
+    decoder :: D.Result Int64
     decoder = D.singleRow (D.value D.int8)
+
+
+selectDocuments :: IO (Maybe [Document])
+selectDocuments = runDB $ query () q
+  where
+    q :: Query () [Document]
+    q = statement sql encoder decoder True
+
+    sql = "select title, author from documents"
+
+    encoder :: E.Params ()
+    encoder = E.unit
+
+    decoder :: D.Result [Document]
+    decoder = D.rowsList $ Document <$> D.value D.text <*> D.value D.text
+
 
 data Document = Document
   { documentTitle  :: Text
   , documentAuthor :: Text
-  }
+  } deriving Show
 
 instance FromForm Document where
   fromForm f = Document
