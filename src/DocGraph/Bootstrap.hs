d0bf2eecbdc7897ea0e6eeddd1015b1253955e62
Mikkel Christiansen <mikkel@rheosystems.com>
2018-04-14 13:14:11 +0200

    Add javascript to bottom of body in applyHead

--- src/DocGraph/Bootstrap.hs
+++ src/DocGraph/Bootstrap.hs
@@ -12,8 +12,17 @@ applyHead body = do
     H.meta ! A.charset "utf-8"
     H.meta ! A.name "viewpoint" ! A.content "width=device_width, intial-scale=1 , shrink-to-fit=no"
     H.link ! A.rel "stylesheet" ! A.href "/static/css/bootstrap.min.css "
-  H.body $
+  H.body $ do
     H.div ! A.class_ "container" $ body
+    H.script ! A.src "https://code.jquery.com/jquery-3.3.1.slim.min.js"
+             ! H.customAttribute "integrity" "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+             ! H.customAttribute "crossorigin" "anonymous" $ ""
+    H.script ! A.src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+             ! H.customAttribute "integrity" "sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+             ! H.customAttribute "crossorigin" "anonymous" $ ""
+    H.script ! A.src "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+             ! H.customAttribute "integrity" "sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+             ! H.customAttribute "crossorigin" "anonymous" $ ""
 
 formGroup :: AttributeValue -> Html -> Maybe Text -> Html
 formGroup fid ftitle mval =
